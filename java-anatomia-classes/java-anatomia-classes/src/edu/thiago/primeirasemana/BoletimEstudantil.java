dfed145f5988897cfced233f59363abeaea39aac
Thiago Bohonos <thiago.bohonos@gmail.com>
2023-01-12 21:14:23 -0300

    Organizando as classes em packages

--- java-anatomia-classes/java-anatomia-classes/src/edu/thiago/primeirasemana/BoletimEstudantil.java
+++ java-anatomia-classes/java-anatomia-classes/src/edu/thiago/primeirasemana/BoletimEstudantil.java
@@ -1,3 +1,4 @@
+package edu.thiago.primeirasemana;
 public class BoletimEstudantil {
     
     public static void main(String[] args) {
--- java-anatomia-classes/java-anatomia-classes/src/edu/thiago/segundasemana/MinhaClasse.java
+++ java-anatomia-classes/java-anatomia-classes/src/edu/thiago/segundasemana/MinhaClasse.java
@@ -1,3 +1,4 @@
+package edu.thiago.segundasemana;
 public class MinhaClasse {
 
     public static void main (String [] args) {
